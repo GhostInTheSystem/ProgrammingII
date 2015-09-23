d9438e789743c5c9e1ab1b02dd7f6b9f5e866c4e
Jeffrey Worley <jeff@theworleys.com>
2015-09-23 14:04:42 -0600

    Create Chapter3Excercises.java

---
+++ Chapter3/Chapter3Excercises.java
@@ -0,0 +1,19 @@
+class Chapter3Excercises{
+	public static void main(String[] args) {
+		/** 
+		R3.16
+		*/
+		/*Counter c1 = new Counter();
+
+		c1.reset();
+		System.out.println(c1.getValue());
+
+		c1.click();
+		System.out.println(c1.getValue());
+		*/
+
+		/**
+		R3.17
+		*/
+	}
+}
